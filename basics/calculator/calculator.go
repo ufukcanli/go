9b54b87d60c0b9c2c70790c53fae75f0e2cd4078
Ufuk Canlı <ufukcanli@hotmail.com>
2025-05-21 18:59:29 +0300

    Add dividing

--- basics/calculator/calculator.go
+++ basics/calculator/calculator.go
@@ -1,5 +1,7 @@
 package calculator
 
+import "errors"
+
 func Add(a, b float64) float64 {
 	return a + b
 }
@@ -10,4 +12,12 @@ func Subtract(a, b float64) float64 {
 
 func Multiply(a, b float64) float64 {
 	return a * b
+}
+
+func Divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("division by zero not allowed")
+	}
+
+	return a / b, nil
 }
